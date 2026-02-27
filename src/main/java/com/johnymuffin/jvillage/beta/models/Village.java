54f304f3fad0f2ca5bb97590c6d69f9c1e1943e4
Garsooon <carson12345678910@hotmail.com>
2026-02-27 09:39:33 +0900

    Add Ice and Snow Melt Flag

--- src/main/java/com/johnymuffin/jvillage/beta/models/Village.java
+++ src/main/java/com/johnymuffin/jvillage/beta/models/Village.java
@@ -534,6 +534,14 @@ public class Village implements ClaimManager {
         this.flags.put(VillageFlags.PREVENT_MUSHROOM_SPREAD, value);
     }
 
+    public boolean isPreventIceSnowMelt() {return this.flags.getOrDefault(VillageFlags.PREVENT_ICE_SNOW_MELT, false);
+    }
+
+    public void setPreventIceSnowMelt(boolean value) {
+        modified = true;
+        this.flags.put(VillageFlags.PREVENT_ICE_SNOW_MELT, value);
+    }
+
     public int getTotalClaims() {
         return getClaims().size();
     }
